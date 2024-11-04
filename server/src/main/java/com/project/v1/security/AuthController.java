6b6095a3c9d0310fdf7d17c33735d2c17eddf85b
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2024-11-04 23:32:14 +0530

    repo client error fix

---
+++ server/src/main/java/com/project/v1/security/AuthController.java
@@ -0,0 +1,15 @@
+package com.project.v1.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping(path = "/login")
+    public void login(){}
+}
